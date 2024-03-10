5e73d23cf8adfe38188211d0decb74d6b2c20881
tribuvan <156603931+cse220001015@users.noreply.github.com>
2024-03-11 00:09:30 +0530

    lab-6

--- Lab-6/fifthquestion/fifth.cpp
+++ Lab-6/fifthquestion/fifth.cpp
@@ -1,14 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N=1e5+8;
+bool vertex_visited[N],edge_visited[N];
+int vertex_discovered[N],vertex_low[N],edge_discovered[N],edge_low[N];
 
-bool articulation_point_dfs(vector<vector<int>>&g,vector<int>&vertex_visited,vector<int>&vertex_discovered,vector<int>vertex_low,int a,int &vertex_time,int p){
-    vertex_visited[a]=1;
+bool articulation_point_dfs(vector<vector<int>>&g,int a,int &vertex_time,int p){
+    vertex_visited[a]=true;
     vertex_discovered[a]=vertex_low[a]= ++vertex_time;
     int c=0;
     for(auto it:g[a]){
         if(!vertex_visited[it]){
             c++;
-            if(articulation_point_dfs(g,vertex_visited,vertex_discovered,vertex_low,it,vertex_time,a)){
+            if(articulation_point_dfs(g,it,vertex_time,a)){
                 return true;
             }
             vertex_low[a]=min(vertex_low[a],vertex_low[it]);
@@ -28,6 +31,26 @@ bool articulation_point_dfs(vector<vector<int>>&g,vector<int>&vertex_visited,vec
     }
 }
 
+bool articulation_edge_dfs(vector<vector<int>>&g,int a,int &edge_time,int p){
+    edge_visited[a]=true;
+    edge_discovered[a]=edge_low[a]= ++edge_time;
+    for(auto it:g[a]){
+        if(!edge_visited[it]){
+            if(articulation_edge_dfs(g,it,edge_time,a)){
+                return true;
+            }
+            edge_low[a]=min(edge_low[a],edge_low[it]);
+            if(edge_low[it]>edge_discovered[a]){
+                return true;
+            }
+        }
+        else if(it!=p){
+             edge_low[a]=min(edge_low[a],edge_discovered[it]);
+        }
+    }
+    return false;
+}
+
 int main(){
     freopen("fifthquesinpt.txt","r",stdin);
     freopen("fifthquesoutpt.txt","w",stdout);
@@ -40,13 +63,13 @@ int main(){
         cin>>a>>b;
         g[a].push_back(b);
     }
-    vector<int>vertex_visited(vertices,0);
-    vector<int>vertex_discovered(vertices,0);
-    vector<int>vertex_low(vertices,0);
+    // vector<bool>vertex_visited(vertices,false);
+    // vector<int>vertex_discovered(vertices,0);
+    // vector<int>vertex_low(vertices,0);
     bool art_point=false;
     int vertex_time=0;
     for(int i=0;i<vertices;i++){
-        if(!vertex_visited[i] && articulation_point_dfs(g,vertex_visited,vertex_discovered,vertex_low,i,vertex_time,-1)){
+        if(!vertex_visited[i] && articulation_point_dfs(g,i,vertex_time,-1)){
             cout<<"It has Articulation points!"<<endl;
             art_point=true;
             break;
@@ -56,13 +79,13 @@ int main(){
         cout<<"It does not contain Articulation points!"<<endl;
     }
 
-    vector<int>edge_visited(vertices,0);
-    vector<int>edge_discovered(vertices,0);
-    vector<int>edge_low(vertices,0);
+    // vector<bool>edge_visited(vertices,0);
+    // vector<int>edge_discovered(vertices,0);
+    // vector<int>edge_low(vertices,0);
     bool art_edge=false;
     int edge_time=0;
     for(int i=0;i<vertices;i++){
-        if(!edge_visited[i] && articulation_edge_dfs(g,edge_visited,edge_discovered,edge_low,i,edge_time,-1)){
+        if(!edge_visited[i] && articulation_edge_dfs(g,i,edge_time,-1)){
             cout<<"It has Articulation edges!"<<endl;
             art_edge=true;
             break;
